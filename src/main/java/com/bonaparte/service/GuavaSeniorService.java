533e835a9c733c17989d63267dd18536a1c80685
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-06-30 17:25:49 +0800

    Guava ratelimit 限流

--- src/main/java/com/bonaparte/service/GuavaSeniorService.java
+++ src/main/java/com/bonaparte/service/GuavaSeniorService.java
@@ -1,7 +1,25 @@
 package com.bonaparte.service;
 
+import com.google.common.util.concurrent.RateLimiter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by yangmingquan on 2018/6/29.
  */
+@Service
 public class GuavaSeniorService {
+    private final static Log log = LogFactory.getLog(GuavaSeniorService.class);
+    RateLimiter rateLimiter = RateLimiter.create(10.0);
+
+    /**
+     * 限流，该函数每秒最多调用10次
+     * */
+    public void functionRatelimit(){
+        rateLimiter.acquire();
+        log.info("获得令牌");
+        log.info(rateLimiter.getRate());
+    }
+
 }
