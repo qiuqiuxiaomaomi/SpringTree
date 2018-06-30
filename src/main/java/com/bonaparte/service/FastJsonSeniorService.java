06cf144270594e54f20b4627a2dd3aa01bdb8f8d
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-06-30 12:38:11 +0800

    JSON  todo:后续会增加对实现代码的阅读与优化

--- src/main/java/com/bonaparte/service/FastJsonSeniorService.java
+++ src/main/java/com/bonaparte/service/FastJsonSeniorService.java
@@ -1,7 +1,33 @@
 package com.bonaparte.service;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.bonaparte.dao.mapper.ChargeMapper;
+import com.bonaparte.entity.Charge;
+import com.bonaparte.util.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * Created by yangmingquan on 2018/6/29.
+ * TODO: 研究json的底层代码实现
  */
 public class FastJsonSeniorService {
+    @Autowired
+    private ChargeMapper chargeMapper;
+
+    public void jsonConvert(){
+        List<Charge> charges = chargeMapper.selectAll();
+        charges.stream()
+                .forEach(x ->{
+                    String strx = JSONObject.toJSONString(x);
+                    System.out.println(strx);
+                });
+        JSONArray jsonArray = JSONArray.parseArray(charges.toString());
+
+        JSONObject jsonObject1 = jsonArray.getJSONObject(0);
+        JSONObject jsonObject2 = jsonArray.getJSONObject(1);
+        JSONObject mergeJson = JsonUtil.merge(jsonObject1, jsonObject2);
+    }
 }
---
+++ src/main/java/com/bonaparte/util/JsonUtil.java
@@ -0,0 +1,40 @@
+package com.bonaparte.util;
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Sets;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.Set;
+
+/**
+ * Created by yangmingquan on 2018/6/30.
+ */
+public class JsonUtil {
+    private static final Log log = LogFactory.getLog(JsonUtil.class);
+
+    public static JSONObject merge(JSONObject json1, JSONObject json2) {
+        JSONObject merged = new JSONObject();
+        try {
+            Set<String> keySet = Sets.newHashSet();
+            if (null != json1) {
+                keySet.addAll(json1.keySet());
+            }
+            if (null != json2) {
+                keySet.addAll(json2.keySet());
+            }
+
+            for (String key : keySet) {
+                if (null != json1 && json1.containsKey(key)) {
+                    merged.put(key, json1.get(key));
+                } else if (null != json2 && json2.containsKey(key)) {
+                    merged.put(key, json2.get(key));
+                }
+            }
+        } catch (JSONException e) {
+            log.error("JsonUtil merge error, msg:" + e.getMessage(), e);
+        }
+        return merged;
+    }
+}
