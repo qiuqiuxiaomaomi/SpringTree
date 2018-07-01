dd139ac59d7c4736a835180049b813220117c573
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-01 11:11:26 +0800

    Guava中的JDK原生集合的扩展支持

--- src/main/java/com/bonaparte/service/ConcurrentService.java
+++ src/main/java/com/bonaparte/service/ConcurrentService.java
@@ -2,8 +2,11 @@ package com.bonaparte.service;
 
 import com.bonaparte.entity.Charge;
 import com.google.common.base.CharMatcher;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Range;
 import com.google.common.collect.Sets;
 import com.google.common.primitives.Ints;
 
@@ -29,5 +32,15 @@ public class ConcurrentService {
         //移除控制字符
         String nocontroll = CharMatcher.JAVA_ISO_CONTROL.removeFrom(abc);
         String abcDigit = CharMatcher.DIGIT.retainFrom(abc);
+        String abcSplit = ",1,2,3,5";
+        Splitter.on(",")
+                .omitEmptyStrings()
+                .split(abcSplit)
+                .toString();
+        Joiner.on(",").join(1,2,3);
+        Joiner.on(",").join(intList);
+
+        //区间
+        Range range = Range.closed(3, 5).span(Range.open(5, 10));
     }
 }
