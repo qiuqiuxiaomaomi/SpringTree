e00bbe053df3f8e65b2ae16658556e590a8f0937
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-01 10:05:23 +0800

    Guava Preconditions 函数参数检测

--- src/main/java/com/bonaparte/service/GuavaSeniorService.java
+++ src/main/java/com/bonaparte/service/GuavaSeniorService.java
@@ -1,9 +1,13 @@
 package com.bonaparte.service;
 
+import com.bonaparte.entity.Charge;
+import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.RateLimiter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by yangmingquan on 2018/6/29.
@@ -22,4 +26,15 @@ public class GuavaSeniorService {
         log.info(rateLimiter.getRate());
     }
 
+    public void preconditionCheck(boolean flag,
+                                  List<Integer> list,
+                                  Charge charge,
+                                  Integer id,
+                                  Integer position){
+        Preconditions.checkArgument(flag);
+        Preconditions.checkNotNull(charge);
+        Preconditions.checkNotNull(id);
+        Preconditions.checkNotNull(list);
+        Preconditions.checkElementIndex(position, list.size(), "not null");
+    }
 }
