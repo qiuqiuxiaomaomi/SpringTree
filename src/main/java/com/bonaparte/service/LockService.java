5b41fe886e8b134f4ddf94d0ca891575db58c3bb
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-01 15:51:23 +0800

    添加zookeeper依赖实现分布式锁

--- src/main/java/com/bonaparte/service/LockService.java
+++ src/main/java/com/bonaparte/service/LockService.java
@@ -1,7 +1,13 @@
 package com.bonaparte.service;
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.springframework.beans.factory.annotation.Value;
 import redis.clients.jedis.JedisCommands;
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 /**
  * Created by yangmingquan on 2018/6/29.
@@ -10,10 +16,17 @@ import javax.annotation.Resource;
  *   2：zookeeper实现分布式锁
  *
  */
-public class LockService {
+public class LockService implements Lock, Watcher {
+    @Value("${zookeeper.ip}")
+    private String zookeeperConfig;
+
     @Resource(name = "jedisCluster")
     private JedisCommands jedis;
 
+    /**
+     * 利用部署多实例的redis实现分布式锁
+     * 开启哨兵redis-sentinel
+     * */
     public void redisAddLock(String key, String value) {
         if (jedis.get(key) == null) {
             jedis.setnx(key, value);
@@ -31,4 +44,38 @@ public class LockService {
     /**
      * 利用Zookeeper实现分布式锁
      * */
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
