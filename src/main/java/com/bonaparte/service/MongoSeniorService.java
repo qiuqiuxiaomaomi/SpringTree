70fd6956d3d504b521e9f1b17514c3c065a1c593
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-01 18:08:24 +0800

    mongo基本操作与配置

--- src/main/java/com/bonaparte/service/MongoSeniorService.java
+++ src/main/java/com/bonaparte/service/MongoSeniorService.java
@@ -3,6 +3,11 @@ package com.bonaparte.service;
 import com.bonaparte.entity.Charge;
 import com.bonaparte.util.BeanUtil;
 import com.bonaparte.util.MongoDao;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.connection.QueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +29,19 @@ public class MongoSeniorService {
         try {
             Map<String, Object> map = BeanUtil.objectToMap(charge);
             mongoDao.save(CHARGEINFO, map);
+            DBObject dbObject = mongoDao.findById(CHARGEINFO, "1");
+            DB db = mongoDao.getMongoTemplate().getDb();
+            DBObject query = new BasicDBObject();
+            DBObject orderBy = new BasicDBObject();
+            query.put("speclibId", 1);;
+            orderBy.put("ctime", -1);
+            DBCursor dbCursor = db.getCollection(CHARGEINFO).find(query);
+            dbCursor.sort(orderBy);
+            dbCursor.skip(10);
+            dbCursor.limit(100);
+            while (dbCursor.hasNext()) {
+                DBObject row = dbCursor.next();
+            }
         }catch (Exception e){
             System.out.println("插入mongodb数据");
         }
--- src/main/java/com/bonaparte/util/BeanUtil.java
+++ src/main/java/com/bonaparte/util/BeanUtil.java
@@ -1,7 +1,10 @@
 package com.bonaparte.util;
 
+import com.google.common.base.Preconditions;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,37 +13,38 @@ import java.util.Map;
  */
 public class BeanUtil {
     public static Object mapToObject(Map<String, Object> map, Class<?> beanClass) throws Exception {
-        if (map == null)
-            return null;
+        Preconditions.checkNotNull(map);
 
         Object obj = beanClass.newInstance();
-
         Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields) {
+        Arrays.stream(fields).forEach(field ->{
             int mod = field.getModifiers();
-            if(Modifier.isStatic(mod) || Modifier.isFinal(mod)){
-                continue;
+            if(!Modifier.isStatic(mod) && !Modifier.isFinal(mod)){
+                field.setAccessible(true);
+                try {
+                    field.set(obj, map.get(field.getName()));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
             }
-
-            field.setAccessible(true);
-            field.set(obj, map.get(field.getName()));
-        }
+        });
 
         return obj;
     }
 
     public static Map<String, Object> objectToMap(Object obj) throws Exception {
-        if(obj == null){
-            return null;
-        }
-
-        Map<String, Object> map = new HashMap<String, Object>();
-
+        Preconditions.checkNotNull(obj);
+        Map<String, Object> map = new HashMap<>();
         Field[] declaredFields = obj.getClass().getDeclaredFields();
-        for (Field field : declaredFields) {
+        Arrays.stream(declaredFields).forEach(field ->{
             field.setAccessible(true);
-            map.put(field.getName(), field.get(obj));
-        }
+                    try {
+                        map.put(field.getName(), field.get(obj));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
 
         return map;
     }
