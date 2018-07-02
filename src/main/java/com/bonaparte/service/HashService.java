d30d3cb5022e27332c4bc8cc32ca6609f70744ec
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-02 16:58:45 +0800

    HashMap, ConcurrentHashMap 锁分段技术

--- src/main/java/com/bonaparte/service/HashService.java
+++ src/main/java/com/bonaparte/service/HashService.java
@@ -1,7 +1,24 @@
 package com.bonaparte.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by yangmingquan on 2018/7/1.
+ * HashMap（非线程安全）， Concurrent有很多锁（使用锁分段技术，每一段一把锁，大大的提高程序的并发性能，安全性）
  */
+@Service
 public class HashService {
+    public void hashCheck(){
+        Map<String, Object> map = new HashMap();
+        map.put("a", 1);
+        map.putIfAbsent("b", 1);
+
+        ConcurrentHashMap<String, Object> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put("a", 1);
+        concurrentHashMap.put("b", 2);
+    }
 }
