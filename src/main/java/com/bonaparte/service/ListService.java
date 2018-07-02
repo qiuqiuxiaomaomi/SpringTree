f5b5c3d7e6ab2d084a977cb6e5b66b5d09369041
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-02 18:13:33 +0800

    Queue队列

--- src/main/java/com/bonaparte/service/ListService.java
+++ src/main/java/com/bonaparte/service/ListService.java
@@ -1,16 +1,25 @@
 package com.bonaparte.service;
 
 import com.google.common.collect.Lists;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by yangmingquan on 2018/7/1.
+ * 需要重点理解Map的内部实现
+ *      1: ArrayList 实现
+ *      2：LinkedList 实现
  */
+@Service
 public class ListService {
 
     public void listBasicOp(){
-        List<Integer> list1 = Lists.newArrayList();
-        List<Integer> list2 = Lists.newArrayList();
+        List<String> list1 = Lists.newArrayList();
+        List<String> list2 = Lists.newArrayList();
+        list1.add(null);
+        list1.add(null);
+        //ArrayList 查询性能好 插入性能差
+        //LinkedList 查询性能差，插入性能好
     }
 }
---
+++ src/main/java/com/bonaparte/service/QueueService.java
@@ -0,0 +1,10 @@
+package com.bonaparte.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yangmingquan on 2018/7/2.
+ */
+@Service
+public class QueueService {
+}
