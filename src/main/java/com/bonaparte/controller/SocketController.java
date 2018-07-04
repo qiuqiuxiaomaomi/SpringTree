5785f9161879da45c38ef7388093f2f71dcae7b1
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-04 10:53:17 +0800

    Socket基本操作

---
+++ src/main/java/com/bonaparte/controller/SocketController.java
@@ -0,0 +1,48 @@
+package com.bonaparte.controller;
+
+import com.bonaparte.service.SocketService;
+import com.bonaparte.util.ControllerUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+/**
+ * Created by yangmingquan on 2018/7/4.
+ */
+@Api(value = "Socket通信基础", description = "Socket通信基础")
+@RestController
+@RequestMapping("/socket")
+public class SocketController {
+    @Autowired
+    private SocketService socketService;
+
+    @ApiOperation(value = "Socket操作接口",notes = "Socket操作接口",httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "")
+    })
+    @RequestMapping("/info")
+    public Object socketInfo() throws IOException {
+        Map<String, Object> map = ControllerUtil.defaultSuccResult();
+        socketService.readFromUrl();
+        return map;
+    }
+
+    @ApiOperation(value = "获取主机信息接口",notes = "获取主机信息接口",httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "")
+    })
+    @RequestMapping("/hostinfo")
+    public Object hostInfo() throws IOException {
+        Map<String, Object> map = ControllerUtil.defaultSuccResult();
+        socketService.InetAddress();
+        return map;
+    }
+}
--- src/main/java/com/bonaparte/service/SocketService.java
+++ src/main/java/com/bonaparte/service/SocketService.java
@@ -2,10 +2,49 @@ package com.bonaparte.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+
 /**
  * Created by yangmingquan on 2018/7/1.
  * Socket 通信基础
  */
 @Service
 public class SocketService {
+    //使用URL读取网页内容
+    public void readFromUrl() throws IOException {
+        try {
+            URL url = new URL("https://zhidao.baidu.com/question/243626243216124124.html");
+            InputStream inputStream = url.openStream();
+            System.out.println(inputStream);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+            System.out.println(inputStreamReader);
+            BufferedReader br = new BufferedReader(inputStreamReader);
+            String data = br.readLine();
+            while (data != null){
+                System.out.println(data);
+                data = br.readLine();
+            }
+            br.close();
+            inputStream.close();
+            inputStreamReader.close();
+        }catch (Exception e){
+
+        }finally {
+
+        }
+    }
+
+    public void InetAddress() throws IOException {
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        System.out.println(inetAddress.getHostAddress());
+        System.out.println(inetAddress.getAddress());
+        System.out.println(inetAddress.getHostName());
+        System.out.println(inetAddress.getCanonicalHostName());
+        System.out.println(inetAddress.isReachable(5));
+    }
 }
