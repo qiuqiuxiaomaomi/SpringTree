11ef8948b818de484e57e95add251cac2bb83c93
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-10 18:00:11 +0800

    添加接口

---
+++ src/main/java/com/bonaparte/controller/NettyController.java
@@ -0,0 +1,15 @@
+package com.bonaparte.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangmingquan on 2018/7/10.
+ */
+@Api(value = "NettyController", description = "Netty API")
+@RestController
+@RequestMapping("/netty")
+public class NettyController {
+
+}
