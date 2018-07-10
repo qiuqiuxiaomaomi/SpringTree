2f20739b21a81b2fe81b3887d2f13b5ee7531ae7
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-10 17:52:45 +0800

    添加jdk接口

---
+++ src/main/java/com/bonaparte/controller/JdkController.java
@@ -0,0 +1,15 @@
+package com.bonaparte.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangmingquan on 2018/7/10.
+ */
+@Api(value = "JdkController", description = "JDK api")
+@RestController
+@RequestMapping("/jdk")
+public class JdkController {
+
+}
