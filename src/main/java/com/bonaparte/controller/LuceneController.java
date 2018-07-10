44a5fa03099fcc5f800361619ff95a7cacbb811e
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-10 16:22:33 +0800

    Lucene全文检索功能修改

--- src/main/java/com/bonaparte/controller/LuceneController.java
+++ src/main/java/com/bonaparte/controller/LuceneController.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +31,18 @@ public class LuceneController {
             @ApiResponse(code = 200,message = "")
     })
     @RequestMapping("/info")
-    public Object luceneInfo() throws IOException {
+    public Object luceneInfo() throws IOException, ParseException, InvalidTokenOffsetsException {
         Map<String, Object> map = ControllerUtil.defaultSuccResult();
         luceneService.indexWriter();
         luceneService.queryCheck("成都");
         luceneService.multiQuery();
         luceneService.prefixQuery();
+        luceneService.fuzzyQuery();
+        luceneService.wildcardQuery();
+        luceneService.queryParser();
+        luceneService.multiFieldQueryParse();
+        luceneService.highlighter();
+        luceneService.analyzerCN();
         return map;
     }
 }
--- src/main/java/com/bonaparte/service/LuceneService.java
+++ src/main/java/com/bonaparte/service/LuceneService.java
@@ -1,10 +1,22 @@
 package com.bonaparte.service;
 
+import com.chenlb.mmseg4j.analysis.ComplexAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
@@ -13,6 +25,7 @@ import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Paths;
 
 /**
@@ -46,7 +59,7 @@ public class LuceneService {
         IndexWriter indexWriter = new IndexWriter(directory, indexWriterConfig);
         Document document = new Document();
         document.add(new StringField("address", "中国四川省成都市高新区", Field.Store.YES));
-        document.add(new StringField("userName", "成都刘德华张学友张靓颖", Field.Store.YES));
+        document.add(new StringField("userName", "成都刘德华张学友张靓颖car", Field.Store.YES));
         document.add(new StringField("nation", "成都", Field.Store.YES));
         indexWriter.addDocument(document);
         indexWriter.commit();
@@ -92,5 +105,95 @@ public class LuceneService {
         Term term = new Term(searchField, "成都");
         Query query = new PrefixQuery(term);
         executeQuery(query);
+    }
+
+    public void fuzzyQuery() throws IOException {
+        String searchField = "userName";
+        Term t = new Term(searchField, "cars");
+        Query query = new FuzzyQuery(t);
+        executeQuery(query);
+    }
+
+    public void wildcardQuery() throws IOException {
+        String searchField ="address";
+        Term term = new Term(searchField, "中国*成都");
+        Query query = new WildcardQuery(term);
+        executeQuery(query);
+    }
+
+    /**
+     * 分词查询
+    */
+    public void queryParser() throws ParseException, IOException {
+        Analyzer analyzer = new IKAnalyzer();
+        String searchField = "userName";
+        QueryParser parser = new QueryParser(searchField, analyzer);
+        Query query = parser.parse("成都");
+        executeQuery(query);
+    }
+
+    /**
+     * 多个字段分词查询
+     * */
+    public void multiFieldQueryParse() throws IOException, ParseException {
+        Analyzer analyzer = new IKAnalyzer();
+        String[] filedStr = new String[]{"userName", "address", "nation"};
+        QueryParser queryParser = new MultiFieldQueryParser(filedStr, analyzer);
+        Query query = queryParser.parse("成都");
+        executeQuery(query);
+    }
+
+    /**
+     * 高亮处理
+     * */
+    public void highlighter() throws ParseException, IOException, InvalidTokenOffsetsException {
+        Analyzer analyzer = new IKAnalyzer();
+        String searchField = "address";
+        String text = "成都";
+        QueryParser parser = new QueryParser(searchField, analyzer);
+        Query query = parser.parse(text);
+        TopDocs topDocs = indexSearcher.search(query, 100);
+
+        SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<span style='color:red'>", "</span>");
+        Highlighter highlighter = new Highlighter(simpleHTMLFormatter, new QueryScorer(query));
+        for(ScoreDoc scoreDoc: topDocs.scoreDocs){
+            Document document = indexSearcher.doc(scoreDoc.doc);
+            TokenStream tokenStream = analyzer.tokenStream("address", new StringReader(document.get("address")));
+            String content = highlighter.getBestFragment(tokenStream, document.get("address"));
+            System.out.println(content);
+        }
+    }
+
+    /**
+     * 打印分词
+     * */
+    public void printAnalyzerDoc(Analyzer analyzer, String text) throws IOException {
+        TokenStream tokenStream = analyzer.tokenStream("address", new StringReader(text));
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        try{
+            tokenStream.reset();
+            while (tokenStream.incrementToken()){
+                System.out.println(charTermAttribute.toString());
+            }
+            tokenStream.end();
+        }catch (Exception e){
+
+        }finally {
+            tokenStream.close();
+            analyzer.close();
+        }
+    }
+
+    public void analyzerCN() throws IOException {
+        Analyzer analyzer = null;
+        String text = "Hadoop Spark 是出自美国洛杉矶的计算机大师的专为大规模数据处理而设计的快速通用的计算引擎";
+        analyzer = new IKAnalyzer();
+        printAnalyzerDoc(analyzer, text);
+
+        analyzer = new ComplexAnalyzer();
+        printAnalyzerDoc(analyzer, text);
+
+        analyzer = new SmartChineseAnalyzer();
+        printAnalyzerDoc(analyzer, text);
     }
 }
