824a95d8bf40bb89356ea0806bfa40575ed67681
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-10 18:15:15 +0800

    Dubbo接口添加

---
+++ src/main/java/com/bonaparte/controller/DubboController.java
@@ -0,0 +1,14 @@
+package com.bonaparte.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangmingquan on 2018/7/10.
+ */
+@Api(value = "DubboController",description = "Dubbo API")
+@RestController
+@RequestMapping("/dubbo")
+public class DubboController {
+}
