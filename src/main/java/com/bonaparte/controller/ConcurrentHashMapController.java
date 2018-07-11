95e08d9b412047e79393329046fed7b5205f2ca9
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-11 09:56:23 +0800

    Mongo接口新增

--- src/main/java/com/bonaparte/controller/ConcurrentHashMapController.java
+++ src/main/java/com/bonaparte/controller/ConcurrentHashMapController.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/bonaparte/controller/MongoController.java
+++ src/main/java/com/bonaparte/controller/MongoController.java
@@ -1,8 +1,16 @@
 package com.bonaparte.controller;
 
+import com.bonaparte.service.MongoSeniorService;
+import com.bonaparte.util.ControllerUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by yangmingquan on 2018/7/10.
@@ -11,4 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mongo")
 public class MongoController {
+    @Autowired
+    private MongoSeniorService mongoSeniorService;
+
+    @ApiOperation(value = "mongodb操作接口",notes = "mongodb操作接口",httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "")
+    })
+    @RequestMapping("/info")
+    public Object getInfo(){
+        Map<String, Object> map = ControllerUtil.defaultSuccResult();
+        mongoSeniorService.testMongo();
+        return map;
+    }
 }
