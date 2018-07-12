e121889253daa93fb8de22ad3f9592183236338c
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-12 17:48:20 +0800

    添加MD5 加解密接口

---
+++ src/main/java/com/bonaparte/controller/CookieSessionController.java
@@ -0,0 +1,16 @@
+package com.bonaparte.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangmingquan on 2018/7/12.
+ */
+@Api(value = "CookieSessionController", description = "Cook,Session API")
+@RestController
+@RequestMapping("/cookiesession")
+public class CookieSessionController {
+
+
+}
---
+++ src/main/java/com/bonaparte/controller/MD5Controller.java
@@ -0,0 +1,15 @@
+package com.bonaparte.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangmingquan on 2018/7/12.
+ */
+@Api(value = "MD5Controller", description = "MD5 操作API")
+@RestController
+@RequestMapping(value = "/md5")
+public class MD5Controller {
+
+}
