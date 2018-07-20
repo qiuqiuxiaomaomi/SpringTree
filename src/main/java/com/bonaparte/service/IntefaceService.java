72b9a632d93517ef6eb50d5a141adc36a7499f73
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-07-20 18:27:35 +0800

    Inteface接口

---
+++ src/main/java/com/bonaparte/service/IntefaceService.java
@@ -0,0 +1,8 @@
+package com.bonaparte.service;
+
+/**
+ * Created by yangmingquan on 2018/7/20.
+ * 接口，class区别
+ */
+public class IntefaceService {
+}
---
+++ src/main/java/com/bonaparte/service/SingletonService.java
@@ -0,0 +1,11 @@
+package com.bonaparte.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yangmingquan on 2018/7/20.
+ * 单例模式
+ */
+@Service
+public class SingletonService {
+}
