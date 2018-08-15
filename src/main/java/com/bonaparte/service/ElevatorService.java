ee278224a13a21666275b54e26c826d62169354f
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-08-15 17:33:11 +0800

    Elevator电梯调度算法实现

---
+++ src/main/java/com/bonaparte/service/ElevatorService.java
@@ -0,0 +1,9 @@
+package com.bonaparte.service;
+
+/**
+ * Created by yangmingquan on 2018/8/15.
+ * 电梯调度算法
+ * 链接: https://blog.csdn.net/GitChat/article/details/81703229
+ */
+public class ElevatorService {
+}
