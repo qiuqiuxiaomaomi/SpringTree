529efb9c1242c9a0c9e80721ac0b376de1f9b510
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-08-15 17:14:26 +0800

    Mysql的分库分表

---
+++ src/main/java/com/bonaparte/service/SeparateMySqlService.java
@@ -0,0 +1,11 @@
+package com.bonaparte.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yangmingquan on 2018/8/15.
+ * Mysql 分库分表
+ */
+@Service
+public class SeparateMySqlService {
+}
