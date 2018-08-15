5fcab190a992860969c3511ea4329693ba079706
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-08-15 17:59:32 +0800

    秒杀系统

---
+++ src/main/java/com/bonaparte/service/SecondKillService.java
@@ -0,0 +1,11 @@
+package com.bonaparte.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by yangmingquan on 2018/8/15.
+ * 秒杀系统
+ */
+@Service
+public class SecondKillService {
+}
